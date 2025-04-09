795599520965fe6d9f5dee2b7000351ef42215a6
cjkim <chanjin5212@gmail.com>
2025-04-09 13:07:35 +0900

    수정

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,16 +5,15 @@ export function Footer() {
           <div className="flex flex-col md:flex-row justify-between">
             <div className="mb-6 md:mb-0">
               <div className="text-xl font-semibold mb-2">workflow@imweb.com</div>
-              <div className="text-xl font-semibold">02.1234.5678</div>
+              <div className="text-xl font-semibold">010-4266-8233</div>
             </div>
             <div className="text-right text-sm">
-              <div className="mb-2">대표: 김영희 | 주소: 서울시 강남구 테헤란로</div>
-              <div className="mb-4">사업자등록번호: 000-00-0000</div>
-              <div>개인정보 처리방침 | 이용약관 | 사업자 정보 확인</div>
+              <div className="mb-2">대표: 김정민 | 주소: 서울시 강남구 테헤란로</div>
+              <div className="mb-4">사업자등록번호: 732-23-01019</div>
             </div>
           </div>
           <div className="mt-16">
-            <h2 className="text-5xl font-bold">Start workflow</h2>
+            <h2 className="text-5xl font-bold">미래환경</h2>
           </div>
         </div>
       </footer>
